777b2dbeac745e8e33873a0dff7ebcd170fa26b6
Chavier <javiercastrorodriguez2005@gmail.com>
2026-05-23 11:37:25 +0200

    Las alertas ahora se gaurdan en mongo, ahora todo es funcional

--- spark_job.py
+++ spark_job.py
@@ -4,17 +4,21 @@ from pymongo import MongoClient
 import os
 import sys
 
+# =========================
+# PYSPARK
+# =========================
+
 os.environ["PYSPARK_PYTHON"] = sys.executable
 os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
+
 # =========================
 # MONGODB
 # =========================
 
 client = MongoClient("mongodb://localhost:27017/")
-
 db = client["smartfridge"]
 
-# Leer datos
+# Leer datos IoT
 data = list(db.raw_data.find())
 
 # =========================
@@ -22,7 +26,6 @@ data = list(db.raw_data.find())
 # =========================
 
 for doc in data:
-
     doc.pop("_id", None)
 
 # =========================
@@ -36,7 +39,7 @@ spark = SparkSession.builder \
 df = spark.createDataFrame(data)
 
 # =========================
-# MEDIA TEMPERATURA
+# TEMPERATURA MEDIA
 # =========================
 
 avg_df = df.groupBy("fridge_id").agg(
@@ -54,5 +57,35 @@ avg_df.show()
 alerts_df = df.filter(
     col("temperature") > 10
 )
+
 print("\n=== ALERTAS TEMPERATURA ===")
+
 alerts_df.show()
+
+# =========================
+# GUARDAR ALERTAS EN MONGO
+# =========================
+
+alerts_list = alerts_df.toPandas().to_dict("records")
+
+db.alerts.delete_many({})
+
+if alerts_list:
+
+    db.alerts.insert_many(alerts_list)
+
+    print(f"\n✅ {len(alerts_list)} alertas guardadas en MongoDB")
+
+# =========================
+# GUARDAR MEDIAS EN MONGO
+# =========================
+
+avg_list = avg_df.toPandas().to_dict("records")
+
+db.processed_data.delete_many({})
+
+if avg_list:
+
+    db.processed_data.insert_many(avg_list)
+
+    print(f"\n✅ {len(avg_list)} medias guardadas en MongoDB")
